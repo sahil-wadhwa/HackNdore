b499d2a0cd7d3c0c149ab413d700a147412c101f
Aditi <aditiadisinha@gmail.com>
2024-07-27 01:26:23 +0530

    RegisterAditi commit

--- project/src/pages/RegisterPage.jsx
+++ project/src/pages/RegisterPage.jsx
@@ -44,36 +44,39 @@ const RegisterPage = () => {
   };
 
   return (
-    <div className="register">
-      <h1>Register</h1>
-      <input
+    <div className= " flex flex-col register items-center justify-center w-screen h-screen ">
+      <h1 className="mb-2 font-serif text-[7vh] uppercase">Register</h1>
+      <div className = " flex flex-col gap-5 justify-center items-center w-[27vw] h-[50vh]">
+       <input className="rounded-lg text-center border-2 h-[7vh] w-[20vw]"
         type="text"
         name="name"
         value={user.name}
         placeholder="Enter your name"
         onChange={handleChange}
-      ></input>
-      <input
+       ></input>
+       <input className="rounded-lg border-2 text-center h-[7vh] w-[20vw]"
         type="text"
-        name="email"
+        name= "email"
         value={user.email}
         placeholder="Enter your Email"
         onChange={handleChange}
       ></input>
-      <input
+      <input className="rounded-lg border-2 text-center h-[7vh] w-[20vw]"
         type="password"
         name="password"
         value={user.password}
         placeholder="Enter your password"
         onChange={handleChange}
       ></input>
-      <input
+      <input className="rounded-lg  border-2 text-center h-[7vh] w-[20vw]"
         type="password"
         name="reEnterPassword"
         value={user.reEnterPassword}
         placeholder="Re-enter your password"
         onChange={handleChange}
       ></input>
+      </div>
+      
       <div className="checkbox-wrapper">
         <input
           type="checkbox"
@@ -86,9 +89,9 @@ const RegisterPage = () => {
           <a href="https://www.google.com">conditions</a>
         </label>
       </div>
-      <button className="button" onClick={register}>Register</button>
+      <button className=" border-2 rounded-lg bg-blue-600 items-center  text-white justify-center flex  w-[10vw] h-[7vh] button" onClick={register}>Register</button>
       <div>or</div>
-      <div className="button" onClick={() => navigate('/login')}>Login</div>
+      <div className="border-2 rounded-lg bg-blue-600 items-center  text-white justify-center flex  w-[8vw] h-[5vh] button" onClick={() => navigate('/login')}>Login</div>
     </div>
   );
 };
