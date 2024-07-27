a934e79fe6a74a71ad3104fa469911c464673d24
Sahil Wadhwa <sahil06.wadhwa@gmail.com>
2024-07-27 18:09:03 +0530

    video_size change

--- project/src/components/Video.jsx
+++ project/src/components/Video.jsx
@@ -4,7 +4,7 @@ import React from 'react';
        render(){
           return(
              <div className="myVideo w-screen h-screen object-fill flex ">
-                 <video width={1920} height={1080} muted={true} className='object-fill z-[-1] absolute' controls={false} loop="true" autostart="true" autoPlay="true" src={require('../assets/indore.mp4')} />
+                 <video width={1920} height={1080*0.9} muted={true} className='object-fill z-[-1] absolute' controls={false} loop="true" autostart="true" autoPlay="true" src={require('../assets/indore.mp4')} />
                   <div className='w-[20vw] h-[50vh] bg-black opacity-85 text-white self-center justify-self-end ml-[70vw] rounded-lg'>HI </div>
             </div>
              )
