d747b7ce45dcac68df753608f0017de346d6d091
Aditi <aditiadisinha@gmail.com>
2024-07-27 15:51:06 +0530

    tender_section commit

--- project/src/sections/Tenders/CurrenTSection.jsx
+++ project/src/sections/Tenders/CurrenTSection.jsx
@@ -1,6 +1,93 @@
+import { useState } from "react";
+
 function CurrentTSection(){
+    const field=["Item Name","Quantity","Weight","Price(per unit)","Sector","Date","Time"]
+    const items=["Item1","Item2","Item3","Item4","Item5"]
+    const [form,showForm]=useState(false);
+
     return (
-      <div></div>
+      <div className="w-screen h-screen overflow-x-hidden">
+        <nav className="bg-white w-screen h-[10vh] flex justify-end items-center pr-4 text-black ">
+          <ul className="flex gap-6 mr-6 justify-between">
+            <li className="">
+              <a href="#" className="text-lg hover:text-gray-300">Raise Tender</a>
+            </li>
+            <li className="">
+              <a href="#" className="text-lg hover:text-gray-300">Current Tenders List</a>
+            </li>
+            <li>
+              <a href="#" className="text-lg hover:text-gray-300">Complaints</a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className=" flex flex-row gap-5  mt-3 p-2 w-[100vw] h-[80vh]  bg-white">
+        
+          <div className=" w-full h-full flex flex-col bg-gray-200 rounded-lg "> 
+            <div className=" w-[100%] h-[12%]  rounded-lg p-2 bg-gray-700">
+              <ul className="flex gap-3 text-white">
+                <div className=" text-black">
+                   <select className=" rounded-lg" defaultValue={"option1"} onChange={()=>showForm(true)}>
+                     <option value="option1" disabled={true}>Items</option>
+                     <option value="Iron">Iron</option>
+                     <option value="stonechips">stonechips</option>
+                     <option value="wood">wood</option>
+                     <option value="mud">mud</option>
+                   </select>
+                </div>
+              </ul>
+
+            </div>
+            <div className={form?"w-full h-full bg-gray-300 rounded-md flex flex-col":"w-full h-full bg-red-300 rounded-md hidden flex-col"}>
+              <div className="flex flex-row justify-evenly">
+                {field.map((option)=>{
+                  return (
+                    <div className="border-2 w-full h-full">{option}</div>
+                  )
+                })}
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="flex flex-row gap-5  mt-3 p-2 w-[100vw] h-[50vh]  bg-white">
+          <div className="w-[40vw] h-[40vh] bg-gray-400 rounded-lg">
+             bidding
+          </div>
+          <div className="w-[60vw] h-[40vh] bg-gray-400 rounded-lg">
+          <div className="p-4">
+            <ul className="">
+              {items.map((item)=>{
+                return (
+                  <div className="w-full h-[5.5vh] m-2 rounded-md border-2 flex flex-row justify-between">
+                    <div>{item}</div>
+                    <div className="flex flex-row gap-2">
+                    <input name="check" type="checkbox"></input>
+                    <label for="check">Accept</label>
+                    </div>
+                    
+                    <div className="flex flex-row gap-2">
+                    <input name="check2" type="checkbox"></input>
+                    <label for="check2">Reject</label>
+                    </div>
+
+                  </div>
+                )
+              })}
+            </ul>
+          </div>
+          </div>
+        </div>
+
+        <div className="bg-teal-400 w-[100vw] h-[100vh]" >
+
+
+        </div>
+
+
+
+      </div>
     )
 }
   
--- project/src/sections/Tenders/GenerateTSection.jsx
+++ project/src/sections/Tenders/GenerateTSection.jsx
@@ -1,7 +1,146 @@
-function GenerateTSection(){
-    return (
-        <div></div>
-    )
+import React, { useState } from 'react';
+
+function GenerateTSection() {
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleUploadFiles = () => {
+    if (file) {
+      // Open system file uploader
+      const fileInput = document.createElement('input');
+      fileInput.type = 'file';
+      fileInput.click();
+    }
+  };
+
+  return (
+    <div className="h-screen flex flex-col">
+      <nav className="bg-white py-4">
+        <div className="container mx-auto p-4 flex justify-between">
+          <h1 className="text-lg font-bold">Generate Tender </h1>
+          <ul className="flex md-5">
+            <li>
+              <a href="/" className="text-black hover:text-gray-600  border-2 bg-white transition duration-300 ease-in-out">
+                Back
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <div className="flex-1 overflow-y-auto p-4">
+        <form className="max-w-md mx-auto p-4 pt-6 pb-8 mb-4 bg-gray-300 rounded shadow-md">
+          <h2 className="text-lg font-bold mb-4">Generate T Section</h2>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="item">
+              Choose Item
+            </label>
+            <select
+              id="item"
+              className="block w-full pl-10  border-2 text-sm text-gray-700"
+              required
+            >
+              <option value="">Select an option</option>
+              <option value="rocks">Rocks</option>
+              <option value="mud">Mud</option>
+              <option value="stone">Stone</option>
+              <option value="iron">Iron</option>
+              <option value="plastic">Plastic</option>
+            </select>
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="pricePerUnit">
+              Price per Unit
+            </label>
+            <input
+              type="number"
+              id="pricePerUnit"
+              className="block w-full pl-10  border-2 text-sm text-gray-700"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="weight">
+              Weight
+            </label>
+            <input
+              type="number"
+              id="weight"
+              className="block w-full pl-10 border-2 text-sm  text-gray-700"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm   font-bold mb-2" htmlFor="numUnits">
+              Number of Units
+            </label>
+            <input
+              type="number"
+              id="numUnits"
+              className="block w-full pl-10 text-sm border-2 text-gray-700"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm  font-bold mb-2" htmlFor="basePrice">
+              Base Price to be Sold
+            </label>
+            <input 
+              type="number"
+              id="basePrice"
+              className="block w-full pl-10 border-2 text-sm text-gray-700"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="biddingTime">
+              Time of Bidding
+            </label>
+            <input
+              type="datetime-local"
+              id="biddingTime"
+              className="block w-full pl-10 text-sm text-gray-700"
+              required
+            />
+          </div>
+          <div className="flex justify-between mb-4 gap-4">
+            <input
+              type="file"
+              id="file"
+              className="hidden"
+              onChange={handleFileChange}
+            />
+            <label
+              htmlFor="file"
+              className="bg-gray-400 hover:bg-black text-white font-bold py-2 px-4 rounded cursor-pointer"
+              onClick={handleUploadFiles}
+            >
+              Upload Files
+            </label>
+            <button
+              type="save"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py2 px-4 rounded w-full"
+            >
+              Save
+            </button>
+            <button
+              type="submit"
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-full"
+            >
+              Submit
+            </button>
+            
+          </div>  
+          <div className='text-black'>Uploaded File: {file!=null?file.name:null}</div>
+  
+        </form> 
+      </div>
+
+    </div>
+  );
 }
 
 export default GenerateTSection;
